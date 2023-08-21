387ceb66d502184ca93398bee8b7fef072d31727
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-08-21 16:36:39 +0530

    Make function in .js file that return random choice for computer
    This getComputerChoice function return random choices ('Rock', 'Paper', 'Scissors') using Math.random() function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+let getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  if (randomNumber === 1) {
+    return "Rock";
+  } else if (randomNumber === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+};
+
+console.log(getComputerChoice());
