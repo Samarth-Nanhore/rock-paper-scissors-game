ee4efeb2519624896d95dfb8c92823db1d82e7c5
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-08-23 14:25:11 +0530

    Add function playRound in .js file
    This function playRound plays a single round of Rock Paper Scissors. The function take two parameters the playerSelection and computerSelection then return a string that declares the winner

--- script.js
+++ script.js
@@ -9,4 +9,41 @@ let getComputerChoice = () => {
   }
 };
 
-console.log(getComputerChoice());
+let playRound = (playerSelection, computerSelection) => {
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    if (playerSelection === "rock") {
+      return "You Win! Rock beat Scissors";
+    } else {
+      return "You Lose! Rock beat Scissors";
+    }
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    if (playerSelection === "paper") {
+      return "You Win! Paper beat Rocks";
+    } else {
+      return " You lose! Paper beat Rocks";
+    }
+  } else if (
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    if (playerSelection === "scissors") {
+      return "You Win! Scissors beat Paper";
+    } else {
+      return "You Lose! Scissors beat Paper";
+    }
+  } else if (
+    (playerSelection === "paper" && computerSelection === "paper") ||
+    (playerSelection === "rock" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "scissors")
+  ) {
+    return "It's draw!";
+  } else {
+    return `Your selection ${playerSelection} is not valid hence no score will consider`;
+  }
+};
