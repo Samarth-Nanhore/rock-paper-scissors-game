2f80c795f2f4729c3328ab4d3dec236a0146788c
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-09-10 16:18:06 +0530

    Create variables to store html DOM elements using .querySelector
    html element with class name .selection-btn nested classes, .counter, .score-container nested classes

---
+++ script.js
@@ -0,0 +1,15 @@
+let selectionBtnElements = document
+  .querySelector(".selection-btn")
+  .querySelectorAll("div");
+
+let gameCountElementText = document.querySelector(".counter").textContent;
+
+let playerScoreElementText = document
+  .querySelector(".score-container")
+  .querySelector(".player")
+  .querySelector(".player-score").textContent;
+
+let computerScoreElementText = document
+  .querySelector(".score-container")
+  .querySelector(".computer")
+  .querySelector(".computer-score").textContent;
