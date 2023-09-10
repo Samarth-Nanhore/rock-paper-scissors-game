5313ac2ea0bd20ef0db62a72a5af8ac180866942
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-09-11 00:57:20 +0530

    Update code in script.js
    - Change name of playRound function to game
    - Create findWinner function that update DOM element textContent and give winner of complete game
    - Create conditional statement in game function to play 5 round of game and update  textContent of some DOM element and called findWinner function
    - Get DOM element class name .end-result into variable called gameEndResultElement

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ let gameResultElementText = document
   .querySelector(".result-container")
   .querySelector(".game-result");
 
+let gameEndResultElement = document
+  .querySelector(".result-container")
+  .querySelector(".end-result");
+
 let getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
   if (randomNumber === 1) {
@@ -35,60 +39,74 @@ selectionBtnElements.forEach((btn) => {
   btn.addEventListener("click", () => {
     let playerSelection = btn.textContent.toLowerCase();
     let computerSelection = getComputerChoice().toLowerCase();
-    gameCount += 1;
-    gameCountElementText.textContent = `This is game ${gameCount}`;
-    if (gameCount >= 5) {
-      gameCountElementText.textContent = `This is game 0`;
-    } else {
-      return playRound(playerSelection, computerSelection);
-    }
+    return game(playerSelection, computerSelection);
   });
 });
 
 let playerScore = 0;
 let computerScore = 0;
 
-let playRound = (playerSelection, computerSelection) => {
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
-  ) {
-    if (playerSelection === "rock") {
-      playerScoreElementText.textContent = ` ${(playerScore += 1)}`;
-      gameResultElementText.textContent = "You Win! Rock beat Scissors";
-    } else {
-      computerScoreElementText.textContent = `${(computerScore += 1)}`;
-      gameResultElementText.textContent = "You Lose! Rock beat Scissors";
-    }
-  } else if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
-  ) {
-    if (playerSelection === "paper") {
-      playerScoreElementText.textContent = ` ${(playerScore += 1)}`;
-      gameResultElementText.textContent = "You Win! Paper beat Rocks";
-    } else {
-      computerScoreElementText.textContent = `${(computerScore += 1)}`;
-      gameResultElementText.textContent = " You lose! Paper beat Rocks";
-    }
-  } else if (
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    if (playerSelection === "scissors") {
-      playerScoreElementText.textContent = `${(playerScore += 1)}`;
-      gameResultElementText.textContent = "You Win! Scissors beat Paper";
-    } else {
-      computerScoreElementText.textContent = `${(computerScore += 1)}`;
-      gameResultElementText.textContent = "You Lose! Scissors beat Paper";
-    }
-  } else if (
-    (playerSelection === "paper" && computerSelection === "paper") ||
-    (playerSelection === "rock" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "scissors")
-  ) {
-    gameResultElementText.textContent = "It's draw!";
+let game = (playerSelection, computerSelection) => {
+  gameCount += 1;
+  gameCountElementText.textContent = `This is game ${gameCount}`;
+  if (gameCount >= 6) {
+    gameCountElementText.textContent = `Refresh to restart the game`;
+    gameResultElementText.textContent = `End`;
+    findWinner();
   } else {
-    gameResultElementText.textContent = `Your selection ${playerSelection} is not valid hence no score will consider`;
+    if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+      if (playerSelection === "rock") {
+        playerScoreElementText.textContent = ` ${(playerScore += 1)}`;
+        gameResultElementText.textContent = "You Win! Rock beat Scissors";
+      } else {
+        computerScoreElementText.textContent = `${(computerScore += 1)}`;
+        gameResultElementText.textContent = "You Lose! Rock beat Scissors";
+      }
+    } else if (
+      (playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+      if (playerSelection === "paper") {
+        playerScoreElementText.textContent = ` ${(playerScore += 1)}`;
+        gameResultElementText.textContent = "You Win! Paper beat Rocks";
+      } else {
+        computerScoreElementText.textContent = `${(computerScore += 1)}`;
+        gameResultElementText.textContent = " You lose! Paper beat Rocks";
+      }
+    } else if (
+      (playerSelection === "paper" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      if (playerSelection === "scissors") {
+        playerScoreElementText.textContent = `${(playerScore += 1)}`;
+        gameResultElementText.textContent = "You Win! Scissors beat Paper";
+      } else {
+        computerScoreElementText.textContent = `${(computerScore += 1)}`;
+        gameResultElementText.textContent = "You Lose! Scissors beat Paper";
+      }
+    } else if (
+      (playerSelection === "paper" && computerSelection === "paper") ||
+      (playerSelection === "rock" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "scissors")
+    ) {
+      gameResultElementText.textContent = "It's draw!";
+    } else {
+      gameResultElementText.textContent = `Your selection ${playerSelection} is not valid hence no score will consider`;
+    }
+  }
+};
+
+let findWinner = () => {
+  if (playerScore > computerScore) {
+    gameEndResultElement.textContent = `Congratulations! You win the game`;
+  } else if (computerScore > playerScore) {
+    gameEndResultElement.textContent = `Computer win the Game`;
+  } else if (computerScore === playerScore) {
+    gameEndResultElement.textContent = `It's draw! `;
+  } else {
+    gameEndResultElement.textContent = `Hence the every selection are not valid`;
   }
 };
