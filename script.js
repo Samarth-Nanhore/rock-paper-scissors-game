d06f185fac72c2927a869c593bec364373f48de0
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-09-11 01:01:25 +0530

    Update name of DOM element variables

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let selectionBtnElements = document
   .querySelector(".selection-btn")
   .querySelectorAll("div");
 
-let gameCountElementText = document.querySelector(".counter");
+let gameCountElement = document.querySelector(".counter");
 
-let playerScoreElementText = document
+let playerScoreElement = document
   .querySelector(".score-container")
   .querySelector(".player")
   .querySelector(".player-score");
 
-let computerScoreElementText = document
+let computerScoreElement = document
   .querySelector(".score-container")
   .querySelector(".computer")
   .querySelector(".computer-score");
 
-let gameResultElementText = document
+let gameResultElement = document
   .querySelector(".result-container")
   .querySelector(".game-result");
 
