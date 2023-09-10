643627ec59a43805b7e462d313a396786f6d2bf3
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-09-10 18:42:00 +0530

    Create function playRound and call it on each btn click event
    playRound function get two arguments playerSelection and computerSelection on each click event and based on logic it updated DOM element textContent
    two variable to count score of player and computer

--- script.js
+++ script.js
@@ -31,6 +31,56 @@ let getComputerChoice = () => {
 
 selectionBtnElements.forEach((btn) => {
   btn.addEventListener("click", () => {
-    let btnTextContext = btn.textContent;
+    let playerSelection = btn.textContent.toLowerCase();
+    let computerSelection = getComputerChoice().toLowerCase();
+    return playRound(playerSelection, computerSelection);
   });
 });
+
+let playerScore = 0;
+let computerScore = 0;
+
+let playRound = (playerSelection, computerSelection) => {
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    if (playerSelection === "rock") {
+      playerScoreElementText.textContent = ` ${(playerScore += 1)}`;
+      gameResultElementText.textContent = "You Win! Rock beat Scissors";
+    } else {
+      computerScoreElementText.textContent = `${(computerScore += 1)}`;
+      gameResultElementText.textContent = "You Lose! Rock beat Scissors";
+    }
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    if (playerSelection === "paper") {
+      playerScoreElementText.textContent = ` ${(playerScore += 1)}`;
+      gameResultElementText.textContent = "You Win! Paper beat Rocks";
+    } else {
+      computerScoreElementText.textContent = `${(computerScore += 1)}`;
+      gameResultElementText.textContent = " You lose! Paper beat Rocks";
+    }
+  } else if (
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    if (playerSelection === "scissors") {
+      playerScoreElementText.textContent = `${(playerScore += 1)}`;
+      gameResultElementText.textContent = "You Win! Scissors beat Paper";
+    } else {
+      computerScoreElementText.textContent = `${(computerScore += 1)}`;
+      gameResultElementText.textContent = "You Lose! Scissors beat Paper";
+    }
+  } else if (
+    (playerSelection === "paper" && computerSelection === "paper") ||
+    (playerSelection === "rock" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "scissors")
+  ) {
+    gameResultElementText.textContent = "It's draw!";
+  } else {
+    gameResultElementText.textContent = `Your selection ${playerSelection} is not valid hence no score will consider`;
+  }
+};
